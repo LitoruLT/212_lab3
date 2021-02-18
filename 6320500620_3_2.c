b33664041fdd387dc4f795e9d469771b20c5201e
Litoru <puwarin2001@gmail.com>
2021-02-18 15:26:07 +0700

    3_2 done

--- 6320500620_3_2.c
+++ 6320500620_3_2.c
@@ -2,15 +2,27 @@
 
 void main()
 {
-    int n=0,i;
+    int n=0,i,k;
     scanf("%d",&n);
-    for(i=0 ; i<n-1 ; i++)
+    int p,a,getinfo[2];
+    int time[48]={0};
+    for(i=0 ; i<n ; i++)
     {
-        for(k=0 ; k<j ; k++)
+        for(k=0; k<2 ; k++)
+            scanf("%d",&getinfo[k]);
+        p=getinfo[0];
+        a=getinfo[1];
+        time[p-1]=time[p-1]+a;
+    }
+    int max=time[0],ans=0;
+    for(i=0 ; i<n ; i++)
+    {
+        if(time[i]>max)
         {
-
-            num[i]=num[i]+get;
+            max=time[i];
+            ans=i;
         }
     }
+    printf("%d %d",ans+1,max);
 
 }
