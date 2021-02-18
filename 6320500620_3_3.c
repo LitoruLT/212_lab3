81f42ff00c979959b88ad2d2bbd5b43188cf2026
Litoru <puwarin2001@gmail.com>
2021-02-18 15:41:13 +0700

    3_3 done

--- 6320500620_3_3.c
+++ 6320500620_3_3.c
@@ -2,5 +2,18 @@
 
 void main()
 {
+    int n,di=2;
+    scanf("%d",&n);
+    while(n>=di)
+    {
+        if(n%di==0)
+        {
+            n=n/di;
+            printf("%d\n",di);
+        }
+        else
+            di++;
 
+    }
+    printf("0");
 }
