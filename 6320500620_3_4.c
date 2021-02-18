9fb09acec0f5bc931b5513c81456a6c7e5cb835f
Litoru <puwarin2001@gmail.com>
2021-02-18 16:00:17 +0700

    3_4 Done

--- 6320500620_3_4.c
+++ 6320500620_3_4.c
@@ -3,5 +3,24 @@
 void main()
 {
 
+    unsigned long long n,check,i;
+    char ans='F';
+        scanf("%llu",&n);
+    check=n;
+    while(ans!='T')
+    {
+        for(i=2; i<check ; i++)
+        {
+            if(check%i==0)
+            {
+                ans='F';
+                break;
+            }
+            else
+                ans='T';
+        }
+        check=check-1;
+    }
+    printf("%d",check+1);
 
 }
